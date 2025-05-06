f8c2b3df00c28c25e22d9d0780473a8b797a9de4
RyeS <Arnonleesakun2@gmail.com>
2025-05-07 03:25:48 +0700

    update Auth Login

--- src/services/userService.tsx
+++ src/services/userService.tsx
@@ -1,8 +1,11 @@
 import axios from "axios";
+
 type user = {
   username: string;
   password: string;
 };
+const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000";
+
 export const loginUser = async (data: user) => {
-  return await axios.post("http://localhost:3000/api/login", data);
+  return await axios.post(`${API_URL}/api/login`, data);
 };
