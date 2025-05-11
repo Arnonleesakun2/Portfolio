900189d7031b51bae8eaa346a400fd2d2affeccf
RyeS <Arnonleesakun2@gmail.com>
2025-05-12 00:58:21 +0700

    updale-filter-search

---
+++ src/app/loading.tsx
@@ -0,0 +1,8 @@
+const loading = () => {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <span className="loading loading-spinner text-primary text-3xl"></span>
+    </div>
+  );
+};
+export default loading;
