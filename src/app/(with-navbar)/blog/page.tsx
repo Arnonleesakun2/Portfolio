c839f91153afff94acc0d8a813a7076d4048fff7
RyeS <Arnonleesakun2@gmail.com>
2025-05-15 01:55:12 +0700

    update-ui&footer

--- src/app/(with-navbar)/blog/page.tsx
+++ src/app/(with-navbar)/blog/page.tsx
@@ -18,14 +18,12 @@ const Blogpage = async ({
   const category = params.category || "";
   const search = params.search || "";
   return (
-    <div className="flex flex-col lg:flex-row gap-8 py-10 ">
+    <div className="flex flex-col lg:flex-row gap-8 py-10">
       <div className="lg:w-1/4">
-        <MotionMain>
-          <div className="space-y-4 sticky top-32">
-            <Searchblog />
-            <Listcategory selected={category} />
-          </div>
-        </MotionMain>
+        <div className="space-y-4 sticky top-32">
+          <Searchblog />
+          <Listcategory selected={category} />
+        </div>
       </div>
       <div className="lg:w-3/4">
         <Listblog category={category} search={search} />
